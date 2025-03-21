a768fbfabd0720ecc1f2faf2e29cd0c6ac22ffc0
vehktaur <victorakhihiero@gmail.com>
2025-03-22 00:36:07 +0100

    refactor navbar component to enhance navigation and add animations for improved user experience

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -2,6 +2,7 @@
 
 import { useNavigation } from '@/hooks/use-navigation';
 import { cn } from '@/lib/utils';
+import { AnimatePresence, motion } from 'motion/react';
 import { useRouter } from 'next/navigation';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa6';
 
@@ -10,35 +11,42 @@ const Navbar = () => {
 
   const { push } = useRouter();
   const prev = () => {
-    push(prevRoute?.href || '');
+    if (prevRoute?.href) push(prevRoute.href);
   };
   const next = () => {
-    push(nextRoute?.href || '');
+    if (nextRoute?.href) push(nextRoute.href);
   };
 
   return (
-    <nav className='fixed bottom-16 left-1/2 z-[999] flex -translate-x-1/2 transform items-center rounded-full border border-zinc-600 bg-black/70 py-3 shadow drop-shadow-xl backdrop-blur-lg ~gap-3/5 ~px-4/5'>
+    <nav className='fixed bottom-16 left-1/2 z-[999] flex ~w-40/48 -translate-x-1/2 items-center justify-evenly rounded-full border border-zinc-600 bg-black/70 py-3 shadow drop-shadow-xl backdrop-blur-lg'>
       <button
         onClick={prev}
         className={cn(
-          'grid place-items-center rounded-full border border-zinc-600 bg-black/50 transition-all duration-300 ~size-5/8 hover:scale-125',
-          { 'scale-0': !prevRoute },
+          'grid place-items-center rounded-full border border-zinc-600 bg-black/50 transition-all duration-300 ~size-6/8 hover:scale-125',
+          { 'cursor-not-allowed': !prevRoute?.href },
         )}
       >
         <FaChevronLeft className='size-2 text-secondary' />
       </button>
 
-      <p className='flex items-center justify-center gap-1 overflow-hidden text-sm font-medium text-secondary transition-all duration-300'>
-        {<currentRoute.Icon className='size-4' />}
+      <AnimatePresence>
+        <motion.p
+          key={currentRoute.href}
+          initial={{ opacity: 0, y: 8, scale: 0.5 }}
+          animate={{ opacity: 1, y: 0, scale: 1 }}
+          exit={{ opacity: 0, y: -8, scale: 0.5 }}
+          className='flex items-center justify-center gap-1 overflow-hidden text-sm font-medium text-secondary'
+        >
+          {<currentRoute.Icon className='size-4' />}
 
-        <span> {currentRoute?.name}</span>
-      </p>
-
+          <span> {currentRoute?.name}</span>
+        </motion.p>
+      </AnimatePresence>
       <button
         onClick={next}
         className={cn(
-          'grid place-items-center rounded-full border border-zinc-600 bg-black/50 transition-all duration-300 ~size-5/8 hover:scale-125',
-          { 'scale-0': !nextRoute },
+          'grid place-items-center rounded-full border border-zinc-600 bg-black/50 transition-all duration-300 ~size-6/8 hover:scale-125',
+          { 'cursor-not-allowed': !nextRoute?.href },
         )}
       >
         <FaChevronRight className='size-2 text-secondary' />
